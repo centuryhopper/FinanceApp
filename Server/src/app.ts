d84b2189314657bac1950b37a28e20e1e38d0b37
centuryhopper <43508492+centuryhopper@users.noreply.github.com>
2025-06-18 14:34:55 -0400

    update

--- Server/src/app.ts
+++ Server/src/app.ts
@@ -3,6 +3,7 @@ import express from 'express'
 import accountRoutes from './routes/account_routes'
 import plaidRoutes from './routes/plaid_routes'
 import rateLimiter from './middlewares/rateLimit'
+import path from 'path'
 
 const app = express()
 
@@ -15,7 +16,15 @@ app.use('/api/account', accountRoutes)
 app.use('/api/plaid', plaidRoutes)
 
 // Serve static files from the 'dist' directory
-app.use(express.static('../dist'));
+// app.use(express.static('../dist'));
+// Serve static files from Vue build
+// console.log(path.join(__dirname, '../dist'));
+app.use(express.static(path.join(__dirname, '../dist')));
+
+// Catch-all route to index.html (for Vue router)
+// app.get('*', (req, res) => {
+//   res.sendFile(path.join(__dirname, '../dist/index.html'));
+// });
 
 export default app
   
