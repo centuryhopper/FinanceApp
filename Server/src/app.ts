1525662bb1172bb744136642693efa01f41cb880
centuryhopper <43508492+centuryhopper@users.noreply.github.com>
2025-06-18 14:16:08 -0400

    update

--- Server/src/app.ts
+++ Server/src/app.ts
@@ -2,7 +2,6 @@
 import express from 'express'
 import accountRoutes from './routes/account_routes'
 import plaidRoutes from './routes/plaid_routes'
-import path from 'path'
 import rateLimiter from './middlewares/rateLimit'
 
 const app = express()
@@ -16,7 +15,7 @@ app.use('/api/account', accountRoutes)
 app.use('/api/plaid', plaidRoutes)
 
 // Serve static files from the 'dist' directory
-app.use(express.static('dist'));
+app.use(express.static('../dist'));
 
 export default app
   
