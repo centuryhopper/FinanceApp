ab85670d8d979781be897dec2bce2d0ee3911d62
centuryhopper <43508492+centuryhopper@users.noreply.github.com>
2025-08-22 10:35:47 -0400

    update

--- Server/Program.cs
+++ Server/Program.cs
@@ -84,7 +84,7 @@ builder.Services.AddControllers().AddJsonOptions(options =>
     options.JsonSerializerOptions.Converters.Add(new DateOnlyJsonConverter());
 });
 
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen(options =>
 {
